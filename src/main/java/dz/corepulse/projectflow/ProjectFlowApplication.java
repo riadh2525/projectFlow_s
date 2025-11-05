fc3882f1134a5f1b80632a2d2ea3630311aa18dd
Riad H <riad@example.com>
2025-11-05 14:20:22 +0100

    Initial commit

---
+++ src/main/java/dz/corepulse/projectflow/ProjectFlowApplication.java
@@ -0,0 +1,13 @@
+package dz.corepulse.projectflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFlowApplication.class, args);
+	}
+
+}
