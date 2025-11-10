a9897efc5284bcc3321f47872d0d20e2b5a412ec
safia.krim <safia.krim@corepulse.fr>
2025-11-10 15:56:31 +0100

    "temporary commit"

--- src/main/java/dz/corepulse/projectflow/mappers/EpicMapper.java
+++ src/main/java/dz/corepulse/projectflow/mappers/EpicMapper.java
@@ -3,9 +3,9 @@ package dz.corepulse.projectflow.mappers;
 import dz.corepulse.projectflow.model.dto.response.EpicResponse;
 import dz.corepulse.projectflow.model.entity.Epic;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.web.bind.annotation.Mapping;
+//import org.springframework.web.bind.annotation.Mapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+//import org.mapstruct.Mapping;
 
 @Mapper(
         componentModel = "spring",
